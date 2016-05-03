3b0373fe415506df032735631afdfc7a6eed5e0a
WarSongK <june0512@gmail.com>
2016-05-04 00:31:41 +0900

    mongo db

--- app.js
+++ app.js
@@ -1,7 +1,16 @@
 var express = require('express');
 var path = require('path');
 var app = express();
+var mongoose = require('mongoose');
 
+mongoose.connect("mongodb://test:dkdlwhdk1!@ds013162.mlab.com:13162/myapp");
+var db = mongoose.connection;
+db.once("open",function(){
+  console.log("DB connected!");
+});
+db.on("error", function(err){
+  console.log("DB ERROR :", err);
+});
 
 app.set("view engine", 'ejs');
 app.use(express.static(path.join(__dirname, 'public')));
