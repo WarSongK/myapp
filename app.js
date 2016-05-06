0c25690c8b25954fef887c2d5887d9311481d87d
WarSongK <june0512@gmail.com>
2016-05-07 02:04:24 +0900

    160507

--- app.js
+++ app.js
@@ -2,7 +2,8 @@ var express = require('express');
 var path = require('path');
 var app = express();
 var mongoose = require('mongoose');
-
+var bodyParser = require('body-parser');
+var methodOverride = require('method-override');
 //mongoose.connect("mongodb://test:dkdlwhdk1!@ds013162.mlab.com:13162/myapp");
 mongoose.connect(process.env.MONGO_DB);
 var db = mongoose.connection;
@@ -13,83 +14,72 @@ db.on("error", function(err){
   console.log("DB ERROR :", err);
 });
 
-var dataSchema = mongoose.Schema({
-  name:String,
-  count:Number
+// model setting
+var postSchema = mongoose.Schema({
+  title: {type:String, requred:true},
+  body: {type:String, requred:true},
+  createdAt: {type:Date, default:Date.now},
+  updatedAt: {type:Date}
 });
-var Data = mongoose.model('data', dataSchema);
-Data.findOne({name:"myData"},function(err,data){
-  if(err) return console.log("Data ERROR:", err);
-  if(!data){
-    Data.create({name:"myData", count:0}, function(err, data){
-      if(err) return console.log("Data ERROR:",err);
-      console.log("Counter initialized :",data);
-    });
-  }
-});
+var Post = mongoose.model('post', postSchema);
 
 
+
+// view setting
 app.set("view engine", 'ejs');
+
+// set middlewares
 app.use(express.static(path.join(__dirname, 'public')));
-
-//var data={count:0};
-//app.get('/', function(req, res){
-//  data.count++;
-//  res.render('my_first_ejs',data);
-//});
-//app.get('/reset', function(req, res){
-//  data.count=0;
-//  res.render('my_first_ejs',data);
-//});
-//app.get('/set/count', function(req, res){
-//  if(req.query.count) data.count=req.query.count;
-//  res.render('my_first_ejs',data);
-//});
-//app.get('/set/:num', function(req, res){
-//  data.count=req.params.num;
-//  res.render('my_first_ejs',data);
-//});
-
-app.get('/', function(req,res){
-  Data.findOne({name:"myData"},function(err,data){
-    if(err) return console.log("Data ERROR:",err);
-    data.count++;
-    data.save(function (err){
-      if(err) return console.log("Data ERROR:", err);
-      res.render('my_first_ejs', data);
-    });
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(methodOverride("_method"));
+// set routes
+app.get('/posts', function(req,res){
+  Post.find({}).sort('-createdAt').exec(function(err,posts){
+    if(err) return res.json({success:false, message:err});
+    //res.json({success:true, data:posts});
+    res.render("posts/index", {data:posts});
   });
 });
-app.get('/reset', function(req,res){
-  setCounter(res,0);
+app.get('/posts/new', function(req,res){
+  res.render("posts/new");
 });
-app.get('/set/count', function(req,res){
-  if(req.query.count) setCounter(res,req.query.count);
-  else getCounter(res);
-});
-app.get('/set/:num', function(req,res){
-  if(req.params.num) setCounter(res,req.params.num);
-  else getCounter(res);
-});
-function setCounter(res,num){
-  console.log("setCounter");
-  Data.findOne({name:"myData"},function(err,data){
-    if(err) return console.log("Data ERROR:",err);
-    data.count=num;
-    data.save(function (err){
-      if(err) return console.log("Data ERROR:", err);
-      res.render('my_first_ejs', data);
-    });
+app.post('/posts', function(req,res){
+  Post.create(req.body.post, function(err,post){
+    if(err) return res.json({success:false, message:err});
+    //res.json({success:true, data:post});
+    res.redirect('/posts');
   });
-}
-function getCounter(res){
-  console.log("getCounter");
-  Data.findOne({name:"myData"},function(err,data){
-    if(err) return console.log("Data ERROR:",err);
-    res.render('my_first_ejs', data);
+});
+app.get('/posts/:id', function(req,res){
+  Post.findById(req.params.id, function(err,post){
+    if(err) return res.json({success:false, message:err});
+    //res.json({suceess:true, data:post});
+    res.render("posts/show", {data:post});
   });
-}
-
+});
+app.get('/posts/:id/edit', function(req,res){
+  Post.findById(req.params.id, function(err,post){
+    if(err) return res.json({success:false, message:err});
+    res.render("posts/edit", {data:post});
+  });
+});
+app.put('/posts/:id', function(req,res){
+  req.body.post.updatedAt=Date.now();
+  Post.findByIdAndUpdate(req.params.id, req.body.post, function(err,post){
+    if(err) return res.json({success:flase, message:err});
+    //res.json({success:true, message:post._id+" updated"});
+    res.redirect('/posts/'+req.params.id);
+  });
+});
+app.delete('/posts/:id', function(req,res){
+  //req.body.post.updatedAt=Date.now();
+  Post.findByIdAndRemove(req.params.id, function(err,post){
+    if(err) return res.json({success:flase, message:err});
+    //res.json({success:true, message:post._id+" deleteded"});
+    res.redirect('/posts');
+  });
+});
 app.listen(3000, function(){
   console.log('Server on!');
 });
--- dummy.js
+++ dummy.js
@@ -5,3 +5,4 @@ git clone https://github.com/WarSongK/myapp.git
 
 http://blog.naver.com/azure0777/220465552731
 http://blog.naver.com/azure0777/220488363644
+http://dog-paw.tistory.com/entry/MEAN-%EC%8A%A4%ED%83%9D-%EA%B5%AC%EC%84%B1-%EC%9A%94%EC%86%8C-MongoDB-Express-AngularJS-Nodejs
